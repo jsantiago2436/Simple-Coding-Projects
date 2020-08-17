d84e9e81db132622ab6530ca0300e553b328732d
jsantiago2436 <69292547+jsantiago2436@users.noreply.github.com>
2020-08-17 02:14:52 -0400

    Create Main.java

---
+++ Java/Linked Lists/PolynomialListA/Main.java
@@ -0,0 +1,15 @@
+/*
+ * Polynomial Doubly Linked List (Beginner)
+ * @author: Jonathan Santiago
+ * 8/17/2020
+ * version(1)
+ */
+public class Main {
+	public static void main(String[] args) {
+		DList<Number> foo = new DList<>();
+		foo.append(3);
+		foo.append(4f);
+		foo.append(3f);
+		System.out.println(foo);
+	}
+}
